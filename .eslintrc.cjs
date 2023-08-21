24448c62e32a1ce75fe6a160cfa2e47ef59b9891
S4nt1nO <Gianluca1996@gmx.de>
2023-08-21 10:12:32 +0200

    netlify integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  env: {browser: true, es2020: true},
+  extends: ['airbnb', 'prettier'],
+  ignorePatterns: ['dist', 'eslintrc.cjs'],
+  parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+  settings: {react: {version: '18.2'}},
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'no-unused-vars': 'off',
+    'prettier/prettier': ['error'],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'import/extensions': 'off'
+  }
+
 }
