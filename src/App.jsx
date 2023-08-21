1486fcde35c0e968ea8b9c18cfcb76159be58ad2
Hectorcgn <124736463+Hectorcgn@users.noreply.github.com>
2023-08-21 13:34:46 +0200

    add landing page , anmimation an klick ongetstart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.scss'
+import Landingpage from './components/pages/Landingpage/Landingpage'
+import GetStarted from './components/pages/GetStarted/GetStarted'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Routes>
+        <Route path="/getstarted" element={<GetStarted />} />
+        <Route path="/landingpage" element={<Landingpage />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
