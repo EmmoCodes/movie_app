2cddab8ed4a151ac365a6b52f5af908321d5b8b6
Hectorcgn <124736463+Hectorcgn@users.noreply.github.com>
2023-08-21 13:58:43 +0200

    mobile nav bar  style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Landingpage from './components/pages/Landingpage/Landingpage'
 import GetStarted from './components/pages/GetStarted/GetStarted'
+import NavbarMobile from './components/shared/NavbarMobile/NavbarMobile'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/getstarted" element={<GetStarted />} />
         <Route path="/landingpage" element={<Landingpage />} />
       </Routes>
+      <NavbarMobile />
     </>
   )
 }
--- src/components/shared/NavbarMobile/NavbarMobile.jsx
+++ src/components/shared/NavbarMobile/NavbarMobile.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import './NavbarMobile.scss'
+import Home from '../../../assets/img/home.svg'
+import Bookmark from '../../../assets/img/Bookmark.svg'
+import Save from '../../../assets/img/Save.svg'
+import Profile from '../../../assets/img/Profile.svg'
 
 function NavbarMobile() {
   return (
-    <div>
-      <h1>navbar mobile</h1>
-    </div>
+    <section className="navbarmobilesection">
+      <ul>
+        <li>
+          <img src={Home} alt="Home Icon" />
+        </li>
+        <li>
+          <img src={Bookmark} alt="Bookmark Icon" />
+        </li>
+        <li>
+          <img src={Save} alt="save Icon" />
+        </li>
+        <li>
+          <img src={Profile} alt="Profile Icon" />
+        </li>
+      </ul>
+    </section>
   )
 }
 
