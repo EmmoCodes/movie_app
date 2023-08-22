e2f73be1298c54038602c4b648cf52e60def25a7
vladcuzub <49867904+vladcuzub@users.noreply.github.com>
2023-08-22 14:05:42 +0200

    search input function not done

--- src/components/pages/MovieList/MovieList.jsx
+++ src/components/pages/MovieList/MovieList.jsx
@@ -8,6 +8,7 @@ import SearchBar from '../../shared/SearchBar/SearchBar.jsx'
 
 function MovieList() {
   const [movieData, setMovieData] = useState([])
+  
   useEffect(() => {
     getAllMovies(
       ` https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=27&api_key=${apiKey}`,
--- src/components/shared/SearchBar/SearchBar.jsx
+++ src/components/shared/SearchBar/SearchBar.jsx
@@ -2,18 +2,53 @@ import React from 'react'
 import '../SearchBar/SearchBar.scss'
 import lens from '../../../assets/img/vector.svg'
 import { Link, NavLink } from 'react-router-dom'
+import { apiKey } from '../../../data/api'
+import { useState, useEffect } from 'react'
+import { getAllMovies } from '../../utils/fetches/movieFetch'
 
 function SearchBar() {
+  const [searchInput, setSearchInput] = useState('')
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    if (searchInput !== ' ') {
+      getAllMovies(
+        `https://api.themoviedb.org/3/search/movie?query=${searchInput}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`,
+        setResults,
+      )
+    }
+  }, [searchInput])
+  console.log(results)
+
+
+  
+  const handleSearch = event => {
+    event.preventDefault()
+    if (searchInput === '') return
+    getAllMovies(
+      `https://api.themoviedb.org/3/search/movie?query=${searchInput}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`,
+      setResults,
+    )
+    console.log(results)
+  }
+
   return (
     <section>
       <div className="search_bar">
-        <form>
+        <form onSubmit={handleSearch}>
           <label htmlFor="search"></label>
-          <input type="text" name="search" id="search" placeholder="Search Movie..." />
+          <input
+            type="text"
+            value={searchInput}
+            name="search"
+            id="search"
+            placeholder="Search Movie..."
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <button className="lens" type="submit">
+            <img src={lens} alt="" />
+          </button>
         </form>
-        <button className="lens">
-          <img src={lens} alt="" />
-        </button>
       </div>
       <div className="genre_buttons">
         <NavLink to="/list" className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
