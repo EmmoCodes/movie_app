0e454b48c4bdccb9155d98d60a9189102935d910
Hectorcgn <124736463+Hectorcgn@users.noreply.github.com>
2023-08-22 11:37:11 +0200

    movie list mobile style

--- src/components/shared/MovieItem/MovieItem.jsx
+++ src/components/shared/MovieItem/MovieItem.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import star from '../../../assets/img/polygon.svg'
 import bookmark from '../../../assets/img/bookmark.svg'
+import punkt from '../../../assets/img/Ellipse.svg'
 import { Link } from 'react-router-dom'
 import { apiKey } from '../../../data/api.js'
 import { getAllMovies } from '../../utils/fetches/movieFetch.js'
@@ -21,7 +22,7 @@ function MovieItem({ movie }) {
   return (
     <div className="movie_frame">
       <Link to={`/details/${movie.id}`}>
-        <img src={`https://image.tmdb.org/t/p/w200${movie.backdrop_path}`} alt="" />
+        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="" />
       </Link>
       <div className="movie_frame_text">
         <div className="frame_text_headline">
@@ -32,9 +33,16 @@ function MovieItem({ movie }) {
           <p>
             <img src={star} alt="Star Icon" />
             {movie.vote_average}
+            <img src={punkt} alt="Punkt Icon" className="punkt" />
           </p>
-          <p>{movie.release_date.slice(0, 4)}</p>
-          <p>· {movieDetails.genres.find(genre => genre.id === 27).name}</p>
+          <p>
+            {movie.release_date.slice(0, 4)}
+            <img src={punkt} alt="Punkt Icon" className="punkt" />
+          </p>
+          <p>
+            {movieDetails.genres.find(genre => genre.id === 27).name}
+            <img src={punkt} alt="Punkt Icon" className="punkt" />
+          </p>
           <p>{movieDetails.runtime} m</p>
         </div>
       </div>
