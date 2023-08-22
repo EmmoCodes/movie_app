a80ff03e0e6f163ee00d717c076eed735ca91a92
S4nt1nO <Gianluca1996@gmx.de>
2023-08-22 14:14:09 +0200

    fixed searchbar bug

--- src/components/shared/SearchBar/SearchBar.jsx
+++ src/components/shared/SearchBar/SearchBar.jsx
@@ -21,8 +21,6 @@ function SearchBar() {
   }, [searchInput])
   console.log(results)
 
-
-  
   const handleSearch = event => {
     event.preventDefault()
     if (searchInput === '') return
@@ -33,49 +31,53 @@ function SearchBar() {
     console.log(results)
   }
 
+  function SearchBar() {
+    const { handleGenreSearch } = useContext(FilterContext)
 
-
-function SearchBar() {
-  const { handleGenreSearch } = useContext(FilterContext)
-
-
-  return (
-    <section className="searchbar_wrapper">
-      <div className="search_bar">
-        <form onSubmit={handleSearch}>
-          <label htmlFor="search"></label>
-          <input
-            type="text"
-            value={searchInput}
-            name="search"
-            id="search"
-            placeholder="Search Movie..."
-            onChange={event => setSearchInput(event.target.value)}
-          />
-          <button className="lens" type="submit">
-            <img src={lens} alt="" />
-          </button>
-        </form>
-      </div>
-      <div className="genre_buttons">
-        <NavLink to="/list" className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
-          <button value="28" onClick={handleGenreSearch}>
-            Action
-          </button>
-        </NavLink>
-        <NavLink to="/list" className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
-          <button value="35" onClick={handleGenreSearch}>
-            Comedy
-          </button>
-        </NavLink>
-        <NavLink to="/list" className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
-          <button value="27" onClick={handleGenreSearch}>
-            Horror
-          </button>
-        </NavLink>
-      </div>
-    </section>
-  )
+    return (
+      <section className="searchbar_wrapper">
+        <div className="search_bar">
+          <form onSubmit={handleSearch}>
+            <label htmlFor="search"></label>
+            <input
+              type="text"
+              value={searchInput}
+              name="search"
+              id="search"
+              placeholder="Search Movie..."
+              onChange={event => setSearchInput(event.target.value)}
+            />
+            <button className="lens" type="submit">
+              <img src={lens} alt="" />
+            </button>
+          </form>
+        </div>
+        <div className="genre_buttons">
+          <NavLink
+            to="/list"
+            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
+            <button value="28" onClick={handleGenreSearch}>
+              Action
+            </button>
+          </NavLink>
+          <NavLink
+            to="/list"
+            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
+            <button value="35" onClick={handleGenreSearch}>
+              Comedy
+            </button>
+          </NavLink>
+          <NavLink
+            to="/list"
+            className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}>
+            <button value="27" onClick={handleGenreSearch}>
+              Horror
+            </button>
+          </NavLink>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default SearchBar
